3a1da73cde59f9573300fa14e02f2b05b437c70a
cyrildoussin <cdoussin@gmail.com>
2009-10-20 09:28:20 +0200

    fix package name and bump up version number

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ import ez_setup
 ez_setup.use_setuptools()
 from setuptools import setup, find_packages
 setup(
-    name = "djano-socialauth",
-    version = "0.1.1",
+    name = "django-socialauth",
+    version = "0.1.2",
     packages = ['socialauth', 'openid_consumer'],
     author = "Usware Technologies",
     author_email = "info@uswaretech.com",
