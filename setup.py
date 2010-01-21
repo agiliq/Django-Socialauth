a46940439a452fe8caf0391a29ff5abec03bcb51
oliland <oliland@oliland-desktop.(none)>
2010-01-21 13:01:32 +0000

    its not zip safe

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@ ez_setup.use_setuptools()
 from setuptools import setup, find_packages
 setup(
     name = "django-socialauth",
-    version = "0.1.2a",
+    version = "0.1.2b",
     packages = find_packages(exclude=['commentor']),
     author = "Usware Technologies",
     author_email = "info@uswaretech.com",
     description = "Allows logging via Facebook, Yahoo, Gmail, Twitter and Openid ",
     url = "http://socialauth.uswaretech.net/",
-    include_package_data = True
+    include_package_data = True,
+    zip_safe = False,
 )
