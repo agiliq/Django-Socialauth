84fe6644ea462af2ddf4bee51f9cca26eb16bc50
Didier Rano <didier.rano@gmail.com>
2010-06-15 23:04:09 -0400

    Change version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "django-socialauth",
-    version = "0.1.2b",
+    version = "0.1.2c",
     packages = ['socialauth',
                 'socialauth/lib',
                 'socialauth/templatetags',
