fc18dbf0301c8abfcf22b79f1f0f30e9fe348ced
Javed Khan <javed@agiliq.com>
2010-07-05 18:06:24 +0530

    no changes in settings.py anyway

--- example_project/settings.py
+++ example_project/settings.py
@@ -10,7 +10,7 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'dev.db'              # Or path to database file if using sqlite3.
+DATABASE_NAME = 'data.db'              # Or path to database file if using sqlite3.
 DATABASE_USER = ''             # Not used with sqlite3.
 DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
